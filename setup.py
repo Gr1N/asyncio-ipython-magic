f79e0000210a0e86eaee8521be69d7c873f7f402
Nikita Grishko <gr1n@protonmail.com>
2016-11-13 00:24:02 +0300

    (∩｀-´)⊃━☆ﾟ.*･｡ﾟ

---
+++ setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name='asyncio-ipython-magic',
+    version='0.0.1',
+    description='An extension for IPython that help to run AsyncIO code in '
+                'your interactive session.',
+    author='Nikita Grishko',
+    author_email='gr1n@protonmain.com',
+    url='https://github.com/Gr1N/asyncio-ipython-magic',
+    py_modules=(
+        'asynciomagic',
+    ),
+    install_requires=(
+        'ipython',
+    ),
+    include_package_data=True,
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Framework :: IPython',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ),
+)
